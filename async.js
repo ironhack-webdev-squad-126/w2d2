2daae4b1fd082e57ffb98d086dec0e278d9adc5e
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-03-26 15:21:08 +0100

    further setup

---
+++ async.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    console.log('Hello')
+}, 1000)
+
+let seconds = 10
+
+// each second, decrease the seconds variable (until 0)
+// also put the seconds on the page every second with DOM manipulation
+console.log('World')
