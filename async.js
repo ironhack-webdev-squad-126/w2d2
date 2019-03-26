ad661eebb73289d04d7a7f67ea1750b8dbb67fa4
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-03-26 15:46:05 +0100

    counter finished

--- async.js
+++ async.js
@@ -7,9 +7,10 @@ let seconds = 10
 // each second, decrease the seconds variable (until 0)
 // also put the seconds on the page every second with DOM manipulation
 const myInterval = setInterval(function() {
-    if (seconds < 0) return clearInterval(myInterval)
+    if (seconds < 0) {
+        document.querySelector('#counter').innerHTML = 'TIME IS UP'
+        return clearInterval(myInterval)
+    }
 
-    // seconds-- <- alternative
-    // console.log(seconds)
-    console.log(--seconds)
+    document.querySelector('#counter').innerHTML = seconds--
 }, 1000)
