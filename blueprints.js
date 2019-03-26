21e11140b995387e90a2d0c2120df0f65966070d
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-03-26 11:57:50 +0100

    constructor parameters

--- blueprints.js
+++ blueprints.js
@@ -1,13 +1,29 @@
 class Building {
-    constructor() {
+    constructor(color) {
         this.type = 'hut'
         this.height = 150
-        this.color = 'brown'
+
+        if (color) {
+            this.color = color
+        } else {
+            this.color = 'brown'
+        }
     }
 }
 
-const firstBuilding = new Building()
+// 1.
+// const firstBuilding = new Building()
+// const secondBuilding = new Building()
 
-firstBuilding.color = 'white'
-console.log('firstBuilding', firstBuilding)
-console.log(firstBuilding.height)
+// firstBuilding.color = 'white'
+// console.log('firstBuilding', firstBuilding)
+// console.log(firstBuilding.height)
+
+// 2.
+const customBuilding = new Building('yellow')
+const customBuildingTwo = new Building('red')
+const defaultBuilding = new Building()
+
+console.log('customBuilding', customBuilding)
+console.log('customBuildingTwo', customBuildingTwo)
+console.log('defaultBuilding', defaultBuilding)
