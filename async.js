f09a7f650e1e14a6ed2b18707006bb3467060df0
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-03-26 15:40:48 +0100

    1st counter iteration

--- async.js
+++ async.js
@@ -1,9 +1,15 @@
-setTimeout(function() {
-    console.log('Hello')
-}, 1000)
+// setTimeout(function() {
+//     console.log('Hello')
+// }, 1000)
 
 let seconds = 10
 
 // each second, decrease the seconds variable (until 0)
 // also put the seconds on the page every second with DOM manipulation
-console.log('World')
+const myInterval = setInterval(function() {
+    if (seconds < 0) return clearInterval(myInterval)
+
+    // seconds-- <- alternative
+    // console.log(seconds)
+    console.log(--seconds)
+}, 1000)
