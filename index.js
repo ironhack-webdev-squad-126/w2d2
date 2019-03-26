83d3e67f38f396ed2446e56e3e649c7ecab645f7
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-03-26 09:59:16 +0100

    stuff

--- index.js
+++ index.js
@@ -4,29 +4,32 @@ const animals = [
         mammal: false,
         size: 100,
         name: 'flamingo',
+        sound: 'croak',
+        makeSound: function() {
+            console.log(`I'm ${this.name} and I am ${this.size / 100}m big.`)
+            console.log(`${this.sound}${this.sound}`)
+            console.log('')
+        },
     },
     {
         feathers: false,
         mammal: true,
         size: 250,
         name: 'mr. dolphin',
+        sound: 'iiiiiii',
     },
     {
         feathers: false,
         mammal: true,
         size: 20,
         name: 'Lana',
+        sound: 'wuff',
     },
     {
         feathers: true,
         mammal: false,
         size: 15000,
         name: 'Reminator (T-Rex)',
+        sound: 'mim',
     },
 ]
-
-function displayAnimalProps(animalArray, propToDisplay) {
-    // DO THAT HERE PLS
-}
-
-displayAnimalProps(animals, 'name')
