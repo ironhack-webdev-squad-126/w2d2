a4243928d674fb7936ba51a6deb8610f6dde6ae5
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-03-26 11:35:54 +0100

    final animal iteration

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function reproduce() {
-    if (this.mammal) {
-        console.log('Heres a baby')
+    const randNum = Math.floor(Math.random() * 100)
+    console.log(this.name)
+    if (randNum > this.survivalRate) {
+        console.log('My offspring died!! NO!!!!')
     } else {
-        console.log('Heres an egg')
+        // in case of survival
+        this.offspringCount++
+
+        if (this.mammal) {
+            console.log('Heres a baby')
+        } else {
+            console.log('Heres an egg')
+        }
+
+        console.log(`I now have ${this.offspringCount} offsprings`)
     }
+
+    console.log('')
 }
 
 function makeSound() {
@@ -11,6 +24,8 @@ function makeSound() {
     console.log(`${this.sound}${this.sound}`)
     console.log('')
 }
+
+const offspringCount = 0
 
 const animals = [
     {
@@ -21,6 +36,8 @@ const animals = [
         sound: 'croak',
         makeSound: makeSound,
         reproduce: reproduce,
+        offspringCount,
+        survivalRate: 20,
     },
     {
         feathers: false,
@@ -30,6 +47,8 @@ const animals = [
         sound: 'iiiiiii',
         makeSound,
         reproduce,
+        offspringCount,
+        survivalRate: 70,
     },
     {
         feathers: false,
@@ -39,6 +58,8 @@ const animals = [
         sound: 'wuff',
         makeSound,
         reproduce,
+        offspringCount,
+        survivalRate: 95,
     },
     {
         feathers: true,
@@ -48,9 +69,19 @@ const animals = [
         sound: 'mim',
         makeSound,
         reproduce,
+        offspringCount,
+        survivalRate: 50,
     },
 ]
 
-animals.forEach(animal => {
-    animal.reproduce()
-})
+for (let year = 0; year < 5; year++) {
+    console.log('YEAR ' + year)
+    animals.forEach(animal => {
+        // animal.offSpringCount = 0 <- also possible
+        animal.reproduce()
+    })
+    console.log('\n')
+}
+// my TAs are awesome<3
+
+console.log(animals)
