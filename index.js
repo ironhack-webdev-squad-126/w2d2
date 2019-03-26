8459f91b72dd74aefe88bbd6b81179471c27a511
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-03-26 09:30:11 +0100

    setup

---
+++ index.js
@@ -0,0 +1,32 @@
+const animals = [
+    {
+        feathers: true,
+        mammal: false,
+        size: 100,
+        name: 'flamingo',
+    },
+    {
+        feathers: false,
+        mammal: true,
+        size: 250,
+        name: 'mr. dolphin',
+    },
+    {
+        feathers: false,
+        mammal: true,
+        size: 20,
+        name: 'Lana',
+    },
+    {
+        feathers: true,
+        mammal: false,
+        size: 15000,
+        name: 'Reminator (T-Rex)',
+    },
+]
+
+function displayAnimalProps(animalArray, propToDisplay) {
+    // DO THAT HERE PLS
+}
+
+displayAnimalProps(animals, 'name')
